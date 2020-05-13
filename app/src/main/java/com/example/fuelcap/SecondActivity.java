bcbbe0c12fba0910e02e1874bbe9e392ec073286
Cristian <x17138736@student.ncirl.ie>
2020-05-13 16:23:53 +0100

    Added Menu

--- app/src/main/java/com/example/fuelcap/SecondActivity.java
+++ app/src/main/java/com/example/fuelcap/SecondActivity.java
@@ -1,9 +1,12 @@
 package com.example.fuelcap;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -20,9 +23,9 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        logout=(Button) findViewById(R.id.btnLogout);
-
         firebaseAuth=FirebaseAuth.getInstance();
+
+        logout=(Button) findViewById(R.id.btnLogout);
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,6 +40,20 @@ public class SecondActivity extends AppCompatActivity {
         firebaseAuth.signOut();
         finish();
         startActivity(new Intent(SecondActivity.this, MainActivity.class));
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logoutMenu:{
+                Logout();
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
