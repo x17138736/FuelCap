52a783f6dd2e83694c5719b9ebe3eba44d44a66d
Stephen Brien <x17141729@student.ncirl.ie>
2020-05-18 12:44:25 +0100

    Initial Language Implementation.

--- app/src/main/java/com/example/fuelcap/MainActivity.java
+++ app/src/main/java/com/example/fuelcap/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.fuelcap;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -18,6 +24,8 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     private EditText Name;
@@ -34,7 +42,21 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        loadLocale();
         setContentView(R.layout.activity_main);
+
+        //change the action bar title.
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(getResources().getString(R.string.app_name));
+        
+        Button changeLang = findViewById(R.id.chang_Lang);
+        changeLang.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //Show the alert box to bring up the display of languages that can be selected.
+                showChangeLanguageDialog();
+            }
+        });
 
         Name = (EditText)findViewById(R.id.etName);
         Password = (EditText)findViewById(R.id.etPassword);
@@ -74,6 +96,60 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void showChangeLanguageDialog() {
+        // Languages that are available to use
+        final String[] listItems = {"Deutsche", "Español", "English"};
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+        mBuilder.setTitle("Choose Your Language..");
+        mBuilder.setSingleChoiceItems(listItems, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (i==0){
+                    // Choose German
+                    setLocale("de");
+                    recreate();
+                }
+                else if (i==1){
+                    // Choose Spanish
+                    setLocale("es");
+                    recreate();
+                }
+                else if (i==2){
+                    // Choose English
+                    setLocale("en");
+                    recreate();
+                }
+
+                //get rid of the dialog box after the language has been selected.
+                dialogInterface.dismiss();
+            }
+        });
+
+        AlertDialog mDialog = mBuilder.create();
+        // let the alert dialog box be shown.
+        mDialog.show();
+    }
+
+    private void setLocale(String lang) {
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config, getApplicationContext().getResources().getDisplayMetrics());
+        // Save Data to shared Preferences
+        SharedPreferences.Editor editor = getSharedPreferences("Settings", MODE_PRIVATE).edit();
+        editor.putString("My_Lang", lang);
+        editor.apply();
+    }
+
+    // Load Language that's saved in shared preferences.
+    public void loadLocale(){
+        SharedPreferences prefs = getSharedPreferences("Settings", Activity.MODE_PRIVATE);
+        String language = prefs.getString("My_Lang", "");
+        setLocale(language);
+    }
+
     private void validate (String userName, String userPassword) {
 
         progressDialog.setMessage("Stay cool until you are verified!");
