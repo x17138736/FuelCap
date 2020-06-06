5be24c61cc208c7a0b819666e3f71a19929bc08c
Alex-Plktt <alex.plktt432@gmail.com>
2020-06-06 12:26:25 +0100

    added design and features of the review page.

--- app/src/main/java/com/example/fuelcap/Review.java
+++ app/src/main/java/com/example/fuelcap/Review.java
@@ -2,13 +2,34 @@ package com.example.fuelcap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RatingBar;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Review extends AppCompatActivity {
+        String feedback="Thank you for your feedback";
+        Button subBtn1;
+        RatingBar andRatingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review);
+
+        final RatingBar andRatingBar = (RatingBar) findViewById(R.id.andRatingBar);
+        Button subBtn1 = (Button) findViewById(R.id.subBtn1);
+
+        subBtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),feedback, Toast.LENGTH_LONG).show();
+                finish();
+                startActivity(new Intent(Review.this, MainActivity.class));
+            }
+        });
     }
 }
