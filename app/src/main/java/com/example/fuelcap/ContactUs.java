e6dfddc76ecbba1a3e41dbc65c05ded37d712f02
Stephen Brien <x17141729@student.ncirl.ie>
2020-06-11 12:52:22 +0100

    Added the basic ContactUs page without functioning button.

--- app/src/main/java/com/example/fuelcap/ContactUs.java
+++ app/src/main/java/com/example/fuelcap/ContactUs.java
@@ -3,6 +3,8 @@ package com.example.fuelcap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ContactUs extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class ContactUs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
+
+        final EditText mail1 = findViewById(R.id.email1);
+        final EditText mail2 = findViewById(R.id.email2);
+        final EditText name =  findViewById(R.id.conName);
+        final EditText cMsg =  findViewById(R.id.cUsMsg);
+        final Button   cBtn =  findViewById(R.id.sndBtn);
     }
 }
